e0fded084fec70ae2f22e65fa47a3ff547535598
unknown <lixinduan@lixinduan-PC0.tencent.com>
2015-04-27 11:15:58 +0800

    ie6问题，从html中拆分出js

---
+++ static/scripts/header.js
@@ -0,0 +1,38 @@
+(function(){
+    var dropdown = $('.dropdown-list-btn');
+
+    dropdown.on('click', function() {
+        var self = $(this);
+        self.find('ul').slideToggle('slow');
+
+        $('#drop-arrow').toggleClass('dropup-arrow');
+
+    });
+
+    $('.nav-menu li').hover(
+        function () {
+            $(this).addClass('cur');
+        },
+        function () {
+            $(this).removeClass('cur');
+        }
+    );
+
+    $('.senond-item').hover(
+        function () {
+            $(this).addClass('nav-hover');
+        },
+        function () {
+            $(this).removeClass('nav-hover');
+        }
+    );
+
+    $('.dropdown-list-btn li').hover(
+        function () {
+            $(this).addClass('store-hover');
+        },
+        function () {
+            $(this).removeClass('store-hover');
+        }
+    );
+})();
